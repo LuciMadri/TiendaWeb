c053ee0349bd3b1310fb2d083ce6fff6c4d602f8
LuciMadri <89278285+LuciMadri@users.noreply.github.com>
2022-08-10 20:28:45 -0600

    pracrica

---
+++ PRACTICALM/src/test/java/com/LM_EXAMEN_II/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LM_EXAMEN_II;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
